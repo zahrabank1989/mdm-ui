a6afc97981925b002503c47200e2f30c9837b802
reza <reza.arjmandi@up2parts.com>
2024-12-22 11:32:53 +0100

    add images with small and medium size

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,17 @@
 // src/components/Login.tsx
 import React from 'react';
-import { Button, TextField, Container, Typography, Box } from '@mui/material';
-
+import {Button, TextField, Container, Typography, Box} from '@mui/material';
 
 const CustomUserIcon = '/login/profile.svg'; // Path to profile.svg in public/login
 const CustomLockIcon = '/login/shield.png'; // Path to lock.png in public/login
-const FooterImage = '/login/power-grids-and-outages-2 1.svg'; // Path to your footer image
-
+const FooterImageLg = '/login/power-grids-and-outages-2 1.svg'; // Path to your footer image
+const FooterImageMd = '/login/power-grids-and-outages-md.svg'; // Path to your footer image
+const FooterImageSm = '/login/power-grids-and-outages-sm.svg'; // Path to your footer image
 const Login: React.FC = () => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log("Login submitted");
     };
-
     return (
         <Container
             component="main"
@@ -24,36 +23,37 @@ const Login: React.FC = () => {
                 justifyContent: 'center', // Center vertically
                 backgroundColor: 'rgba(233, 237, 248, 1)',
                 padding: 0,
-                margin:0,
-               // width:'100vw'
-               // position: 'relative', // Ensure footer can be positioned absolutely
-
-
-
+                margin: 0,
             }}
             disableGutters
             maxWidth={false}
         >
             <Box
                 sx={{
-                    backgroundColor: 'rgba(245, 248, 255, 1)',
+                    backgroundColor: 'rgba(0, 248, 255, 1)',
                     borderRadius: 2,
                     padding: 3,
-                    width: '100%', // Fill width of the container
-                    maxWidth: '400px', // Max width for the inner Box
+                    width: {
+                        xs: '80%',  // 90% width on extra small screens
+                        sm: '70%',   // 70% width on small screens
+                        md: '50%',   // 50% width on medium screens
+                        lg: '40%',   // 40% width on large screens
+                    },
+                    maxWidth: '400px', // Optional: Set a maximum width for larger screens
                     boxShadow: 1, // Optional: Add shadow for better visibility
+
                 }}
             >
                 <Typography component="h1" variant="h5" align="center">
                     Sign In
                 </Typography>
                 <form onSubmit={handleSubmit}>
-                    <Box sx={{ mb: 2 }}>
-                        <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Box sx={{mb: 2}}>
+                        <Typography variant="body1" sx={{display: 'flex', alignItems: 'center'}}>
                             <img
                                 src={CustomUserIcon}
                                 alt="User Icon"
-                                style={{ marginRight: '8px', width: '24px', height: '24px' }}
+                                style={{marginRight: '8px', width: '24px', height: '24px'}}
                             />
                             Username
                         </Typography>
@@ -66,12 +66,12 @@ const Login: React.FC = () => {
                             placeholder="Please Enter user name"
                         />
                     </Box>
-                    <Box sx={{ mb: 2 }}>
-                        <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Box sx={{mb: 2}}>
+                        <Typography variant="body1" sx={{display: 'flex', alignItems: 'center'}}>
                             <img
                                 src={CustomLockIcon}
                                 alt="Lock Icon"
-                                style={{ marginRight: '8px', width: '24px', height: '24px' }}
+                                style={{marginRight: '8px', width: '24px', height: '24px'}}
                             />
                             Password
                         </Typography>
@@ -88,13 +88,12 @@ const Login: React.FC = () => {
                         type="submit"
                         fullWidth
                         variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
+                        sx={{mt: 3, mb: 2}}
                     >
                         Sign In
                     </Button>
                 </form>
             </Box>
-
             {/* Footer Image */}
             <Box
                 sx={{
@@ -104,13 +103,20 @@ const Login: React.FC = () => {
                     right: 0,
                     display: 'flex',
                     justifyContent: 'center',
+                    mt: 10,
                 }}
             >
-                <img
-                    src={FooterImage}
-                    alt="Footer"
-                    style={{ maxWidth: '100%', height: 'auto' }} // Responsive image
-                />
+                <picture>
+                    <source media="(max-width: 600px)" srcSet={FooterImageSm}/>
+                    <source media="(max-width: 960px)" srcSet={FooterImageMd}/>
+                    <img
+                        src={FooterImageLg}
+                        alt="Footer"
+                        style={{maxWidth: '100%', height: 'auto'}} // Responsive image
+                    />
+                </picture>
+
+
             </Box>
         </Container>);
 };
